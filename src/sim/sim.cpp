590e624cf55c36e4b042feb8e950a25e83c64947
名字 <@stu.hit.edu.cn>
2023-03-03 18:23:41 +0800

    feat: 更新许可证信息

--- src/sim/sim.cpp
+++ src/sim/sim.cpp
@@ -1,4 +1,14 @@
-﻿#include <string>
+﻿/** Copyright (c) 2023 Jim-shop
+ * bp is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ */
+#include <string>
 #include <random>
 #include <fstream>
 #include <iostream>
@@ -29,7 +39,7 @@ template <typename T>
     return stream;
 }
 
-template<>
+template <>
 ::std::ostream &operator<<(::std::ostream &stream, ::std::vector<uint8_t> const &vec)
 {
     stream << '[';
